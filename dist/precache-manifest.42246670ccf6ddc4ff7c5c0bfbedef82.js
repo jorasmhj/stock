55b821c02c14c23c8cbc880cc981940710fbc97f
saroj <saroj@accessibleict.com>
2019-01-17 16:01:45 +0545

    changes

---
+++ dist/precache-manifest.42246670ccf6ddc4ff7c5c0bfbedef82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bd97b162b633eb63d0b4",
+    "url": "/js/chunk-vendors.4ede8c13.js"
+  },
+  {
+    "revision": "a2ef98cb8adf60ac124b",
+    "url": "/js/app.7caf727d.js"
+  },
+  {
+    "revision": "0f3c33cd3342b2770030",
+    "url": "/js/about.d63ad26f.js"
+  },
+  {
+    "revision": "dda8889fafcb6c40c98d825bae028b99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a2ef98cb8adf60ac124b",
+    "url": "/css/app.929070c5.css"
+  }
+];
