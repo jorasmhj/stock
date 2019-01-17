b6a213c3ae4a6a4c276c802bb2698b5a9e02cc2e
joras <jorasmhj@gmail.com>
2019-01-17 21:57:19 +0545

    fix

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const path = require("path");
-const compression = require("compression");
 const app = express();
-
-app.use(compression());
 
 app.use(express.static(__dirname + "/dist"));
 
